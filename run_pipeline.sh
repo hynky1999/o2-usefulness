fcdf6183abe3eeeebbb7942c2b91678f5e20835b
Hynek Kydlíček <kydliceh@aic.aic.hide.ms.mff.cuni.cz>
2023-06-06 23:41:28 +0200

    config and pipeline

---
+++ run_pipeline.sh
@@ -0,0 +1,3 @@
+python -m etl.run --config etl_config.json && \
+python -m etl.run --config etl_test_config.json && \
+python -m nbconvert --to html --no-input --execute --output-dir=reports/ --output=report.html explore/exploration.ipynb
